d470c9bf6f465c3bed56341a6b32406e5480ddf8
ArtemIvanov <Ivanov.Ar.V@sblogistica.ru>
2021-07-14 13:06:20 +0300

    initial commite. add gin router

---
+++ main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	router := gin.Default()
+
+	router.POST("/employee")
+	router.GET("employee/:id")
+	router.PUT("employee/:id")
+	router.DELETE("employee/:id")
+
+	router.Run()
+}
